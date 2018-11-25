600c427cac939a301f2da67c4df9bd3d2a947fdb
ncam <ncampbell@mac.com>
2018-11-25 08:13:53 -0800

    Add files via upload

---
+++ views/layout.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+    <link href="<?php echo $this->make_route('/verge/css/master.css') ?>" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <h1>Verge</h1>
+        <?php include($this->content); ?>
+    </body>
+</html>
---
+++ views/signup.php
@@ -0,0 +1,8 @@
+Signup Now!
+
+<form action="<?php echo $this->make_route('/verge/signup') ?>"
+      method="post">
+    <label for="name">Name</label>
+    <input id="name" name="name" type="text"> <br />
+    <input type="Submit" value="Submit">
+</form>
